571001d8a461221287b83c6a849023786c98f541
Sandy <sandy51529@gmail.com>
2020-12-15 10:21:42 +0800

    模糊化處理測試

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from PIL import Image
 import time
+import pytesseract
+import cv2
 
 # 取消彈出視窗
 # options = Options()
@@ -12,16 +14,23 @@ def get_captcha(driver):
     element = driver.find_elements_by_tag_name("img")
     img = element[1]
     img.screenshot("captcha.png")
-
-
+    # img = Image.open("captcha.png")
+    # pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    # text = pytesseract.image_to_string(img, lang='eng')
+    # print(type(text))
+    # print(text+"?")
+    img = cv2.imread("captcha.png", 0)
+    img = cv2.medianBlur(img, 5)
+    cv2.imshow("newimg", img)
+    cv2.waitKey(0)
+    pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    text = pytesseract.image_to_string(img, lang="eng")
+    print(text)
 
 driver = webdriver.Chrome()
-driver.set_window_size(1024, 960)
-# driver.maximize_window()
 driver.get("https://caselearn2.fju.edu.tw/cas/login?service=https%3A//elearn2.fju.edu.tw/login%3Fnext%3D/user/index&locale=zh_TW&ts=1607908465.74927")
 get_captcha(driver)
 
 username = driver.find_element_by_id("username")
 password = driver.find_element_by_id("password")
 captcha = driver.find_element_by_id("captcha")
-
