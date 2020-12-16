3911c763043e9587ce6b22e84efa5b41b6540232
Sandy <sandy51529@gmail.com>
2020-12-16 22:18:09 +0800

    程式優化

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ username = driver.find_element_by_id("username")
 password = driver.find_element_by_id("password")
 captcha = driver.find_element_by_id("captcha")
 
-username.send_keys("408040143")
-password.send_keys("sandy010419")
+username.send_keys("")
+password.send_keys("")
 captcha.send_keys(guess)
 driver.find_element_by_class_name("btn-submit").click()
 
