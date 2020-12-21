5237910c8870aa74386d681696318566ebedb098
Sandy <sandy51529@gmail.com>
2020-12-21 16:57:02 +0800

    程式優化

--- main.py
+++ main.py
@@ -5,22 +5,44 @@ import test
 import pandas as pd
 import csv
 
+def login(driver, ac, pa):
+    driver.find_element_by_id("username").clear()
+    driver.find_element_by_id("password").clear()
+    username = driver.find_element_by_id("username")
+    password = driver.find_element_by_id("password")
+    username.send_keys(ac)
+    password.send_keys(pa)
 
-driver = webdriver.Chrome()
+def capt(driver):
+    func.get_captcha(driver)
+    guess = func.number()
+    driver.find_element_by_id("captcha").clear()
+    captcha = driver.find_element_by_id("captcha")
+    captcha.send_keys(guess)
+    driver.find_element_by_class_name("btn-submit").click()
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=options, executable_path='./chromedriver')
+# driver = webdriver.Chrome()
 driver.get("https://caselearn2.fju.edu.tw/cas/login?service=https%3A//elearn2.fju.edu.tw/login%3Fnext%3D/user/index&locale=zh_TW&ts=1607908465.74927")
 
-func.get_captcha(driver)
-guess = func.number()
-
-username = driver.find_element_by_id("username")
-password = driver.find_element_by_id("password")
-captcha = driver.find_element_by_id("captcha")
-
 ac, pa = test.start()
-username.send_keys(ac)
-password.send_keys(pa)
-captcha.send_keys(guess)
-driver.find_element_by_class_name("btn-submit").click()
+login(driver, ac, pa)
+capt(driver)
+
+title = driver.title
+times = 0
+while title == "天主教輔仁大學 - 登入 Tronclass":
+    if times <= 2:
+        login(driver, ac, pa)
+        capt(driver)
+    else:
+        ac, pa = test.start()
+        login(driver, ac, pa)
+        capt(driver)
+    title = driver.title
+    times += 1
 
 driver.get("https://elearn2.fju.edu.tw/bulletin-list/")
 
@@ -31,6 +53,7 @@ info_dic = {"title":[], "class":[], "content":[]}
 title_class = driver.find_elements_by_css_selector("span[ng-bind='bulletin.title']")
 for i in title_class:
     info_dic["title"].append(i.text)
+
 class_name = driver.find_elements_by_css_selector("span[ng-bind='getCourseName(bulletin.course_id)']")
 for i in class_name:
     info_dic["class"].append(i.text)
@@ -38,14 +61,10 @@ for i in class_name:
 op = driver.find_elements_by_css_selector("div.bulletin-update-info")
 for i in op:
     i.click()
-
 contents = driver.find_elements_by_css_selector("div.content-review.ng-isolate-scope")
 for content in contents:
     info_dic["content"].append(content.text)
 
-# print(info_dic["title"])
-# print(info_dic["class"])
-# print(info_dic["content"])
 driver.close()
 
 anouce = pd.DataFrame(info_dic)
